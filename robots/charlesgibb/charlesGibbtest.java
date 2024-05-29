2ca48bd91a297382dca231f8d2b9b34f082d29dc
Charles Gibb <charlesgibb99@gmail.com>
2024-05-29 12:09:45 -0400

    This is a new version using primarily just the tra cking stuff, so idk how well it'll work, I'll figure out movement later

--- robots/charlesgibb/charlesGibbtest.java
+++ robots/charlesgibb/charlesGibbtest.java
@@ -1,35 +1,69 @@
 package charlesgibb;
 
-import robocode.*;
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+import robocode.WinEvent;
+import static robocode.util.Utils.normalRelativeAngleDegrees;
 
 import java.awt.*;
-import robocode.Robot;
+
 
 public class charlesGibbtest extends Robot{
+    int i = 0; //this is taken from Tracker.java because it has really good tracking
+    int count = 0;
+    
     public void run() {
-        while (true) {
-            turnGunRight(360); // Continuously scan for enemies
-        }
+        // Prepare gun
+		setAdjustGunForRobotTurn(true); // Keep the gun still when we turn
+		i = 10; // Initialize i to 10
+
+		// Loop forever
+		while (true) {
+			// turn the Gun (looks for enemy)
+			turnGunRight(i);
+			// Keep track of how long we've been looking
+			count++;
+			// If we've haven't seen our target for 2 turns, look left
+			if (count > 2) {
+				i = -30;
+			}
+			// If we still haven't seen our target for 5 turns, look right
+			if (count > 5) {
+				i = 30;
+		    }}
     }
     
     public void onScannedRobot(ScannedRobotEvent e) {
-        // Fire at the enemy with increased power as it gets closer
-        fireAtEnemy(e.getDistance());
-        double gunTurnAmt = normalRelativeAngleDegrees(e.getBearing() + (getHeading() - getRadarHeading()));
-        scan();
-        fire(turnGunAmt);
-    }
+       // This part is taken from Tracker just cause it's so frigging good.
+		double absoluteBearing = getHeading() + e.getBearing();
+		double bearingFromGun = normalRelativeAngleDegrees(absoluteBearing - getGunHeading());
+
+		// If it's close enough, fire!
+		if (Math.abs(bearingFromGun) <= 3) {
+			turnGunRight(bearingFromGun);
+			if (getGunHeat() == 0) {
+				fire(Math.min(3 - Math.abs(bearingFromGun), getEnergy() - .1));
+			}
+		} // otherwise just set the gun to turn.
+		// Note:  This will have no effect until we call scan()
+		else {
+			turnGunRight(bearingFromGun);
+	}}
+
+        
+    
     
     public void onHitByBullet(HitByBulletEvent e) {
-        // Dodge the bullet by moving randomly
-        back(100);
-        turnRight(90);
+       
+        turnGunRight(90);
     }
     
-    public void onHitWall(HitWallEvent e) {
+    public void onHitWall() {
         // Avoid the wall by moving away from it
-        back(50);
-        turnRight(90);
+        back(70);
+        turnRight(50);
     }
     
     // Method to turn the gun towards the last scanned enemy
